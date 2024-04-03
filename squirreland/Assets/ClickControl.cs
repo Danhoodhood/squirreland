08d15f8136a9d892420e84595e238fb550f3d7d9
YaroslavNekrasov <yaroslav.nekrasov001@mail.ru>
2024-04-03 21:24:40 +0300

    search for acorns

---
+++ squirreland/Assets/ClickControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickControl : MonoBehaviour
+{
+    public static string obj_name;
+    public static int count;// количество желудей 
+    public Text text;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()//при клике
+    {
+        obj_name = gameObject.name;
+        //Debug.Log(obj_name);
+
+        if (count < 4)//если количество желудей меньше 4х
+        {
+            audioSource.Play();//звук нажатия на желудь
+            Destroy(gameObject);//удаляется желудь
+            count++;
+            text.text = count.ToString();//вывод найденных желудей
+            
+        }
+    }
+
+}
