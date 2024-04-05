bf4841e5365b050b5d219105d61e160101dd3303
YaroslavNekrasov <yaroslav.nekrasov001@mail.ru>
2024-04-05 11:08:42 +0300

    heart System

--- squirreland/Assets/HeartSystem.cs
+++ squirreland/Assets/HeartSystem.cs
@@ -6,48 +6,52 @@ public class HeartSystem : MonoBehaviour
 {
     public static int health;
     public GameObject Heart1, Heart2, Heart3;
-    
-    // Start is called before the first frame update
-    void Start()//при старте игры 3 жизни
+    public AudioSource audioSource;
+
+    void Start()
     {
         Heart1.SetActive(true);
         Heart2.SetActive(true);
         Heart3.SetActive(true);
         health = 3;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-     
-        switch(health)
+        switch (health)
         {
-
-            case 3://все три жизни
+            case 3:
                 Heart1.SetActive(true);
                 Heart2.SetActive(true);
                 Heart3.SetActive(true);
                 break;
 
-            case 2://две жизни
+            case 2:
                 Heart1.SetActive(true);
                 Heart2.SetActive(true);
                 Heart3.SetActive(false);
                 break;
 
-            case 1://одна жизнь 
+            case 1:
                 Heart1.SetActive(true);
                 Heart2.SetActive(false);
                 Heart3.SetActive(false);
                 break;
 
-            case 0://нет ни одной жизни
+            case 0:
                 Heart1.SetActive(false);
                 Heart2.SetActive(false);
                 Heart3.SetActive(false);
                 break;
         }
+    }
 
+    public void DecreaseHealth()
+    {
+        health--;
+
+        // Воспроизводим звук при изменении здоровья
+
+        audioSource.Play();
     }
 }
