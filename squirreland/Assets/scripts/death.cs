8489842a4eb0c257b69652b80405c81f5da6401a
AngelinaDzhgerenaya <124816373+AngelinaDzhgerenaya@users.noreply.github.com>
2024-05-14 21:47:17 +0300

    2

---
+++ squirreland/Assets/scripts/death.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class death : MonoBehaviour
+{
+    public GameObject heart;
+
+    private void Update()
+    {
+        
+    }
+}
